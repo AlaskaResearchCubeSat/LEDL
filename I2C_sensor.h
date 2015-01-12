813403990067af4a5bce5d26ea6996cc6077f6db
Jesse Frey <jmfrey@alaska.edu>
2015-01-12 11:33:11 -0900

    Removed LTC24XX defines from I2C_sensor.h

--- I2C_sensor.h
+++ I2C_sensor.h
@@ -19,60 +19,8 @@
 #define REG_TEMP_HYST     0x02
 #define REG_TEMP_OIT      0x03
 
-// Defined Magnetometer addresses
-#define MAG_SENSORS       6
-#define MAG_64_GAIN       0x85
-#define MAG_1_GAIN        0x80
-#define MAG_X_PLUS_ADDR        0x14
-#define MAG_X_MINUS_ADDR       0x16
-#define MAG_Y_PLUS_ADDR       0x26
-#define MAG_Y_MINUS_ADDR       0x34
-#define MAG_Z_PLUS_ADDR        0x25
-#define MAG_Z_MINUS_ADDR       0x24
-
 // Define Clyde Board Address
 #define clyde_sensors     0x01
-
-#ifndef __LTC24XX_H
-#define __LTC24XX_H
-
-#define LTC24XX_EN      0x20
-#define LTC24XX_PRE     0x80
-#define LTC24xx_SGL     0x10
-#define LTC24xx_SIGN    0x08
-#define LTC24xx_CH_0    0x00
-#define LTC24xx_CH_1    0x01
-#define LTC24xx_EN2     0x80
-#define LTC24xx_IM      0x40
-#define LTC24xx_FA      0x20
-#define LTC24xx_FB      0x10
-#define LTC24xx_SPD     0x08
-#define LTC24xx_GS2     0x04
-#define LTC24xx_GS1     0x02
-#define LTC24xx_GS0     0x01
-
-#define LTC24xx_GAIN1       (0)
-#define LTC24xx_GAIN4       (LTC24xx_GS0)
-#define LTC24xx_GAIN8       (LTC24xx_GS1)
-#define LTC24xx_GAIN16      (LTC24xx_GS1|LTC24xx_GS0)
-#define LTC24xx_GAIN32      (LTC24xx_GS2)
-#define LTC24xx_GAIN64      (LTC24xx_GS2|LTC24xx_GS0)
-#define LTC24xx_GAIN128     (LTC24xx_GS2|LTC24xx_GS1)
-#define LTC24xx_GAIN264     (LTC24xx_GS2|LTC24xx_GS1|LTC24xx_GS0)
-
-//address for all LTC24XX ADC's
-#define LTC24XX_GLOBAL_ADDR 0x77
-
-  //CA1    CA0    ADDRESS
-  //LOW    LOW    0X14
-  //LOW    HIGH   0X16
-  //LOW    FLOAT  0x15
-  //HIGH   LOW    0x26
-  //HIGH   HIGH   0x34
-  //HIGH   FLOAT  0x27
-  //FLOAT  LOW    0x17
-  //FLOAT  HIGH   0x25
-  //FLOAT  FLOAT  0x24
 
 
 ////////using the old clyde user guide for the following addresses, assumming Y1 is Y+ and Y2 is Y-, new eps has pair votlage, i am just choosing + side voltage 
@@ -106,11 +54,6 @@
   #define EPS_HEATER_COMMAND          5
   #define EPS_GPIO_STATUS_COMMAND     6
   #define EPS_WATCHDOG_COMMAND        128
-
-
-
-#endif
-
 
 
 
